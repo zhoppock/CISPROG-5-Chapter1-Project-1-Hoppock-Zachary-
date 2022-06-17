f7ace1872958fb3725ab04d6e05e29863e23ca01
Zachary Hoppock <61926087+zhoppock@users.noreply.github.com>
2022-06-17 16:28:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+print("a. ", end = "")
+print(8)
+
+print("b. ", end = "")
+print(8 * 2)
+
+print("c. ", end = "")
+print(8 ** 2)
+
+print("d. ", end = "")
+print(8/12)
+
+print("e. ", end = "")
+print(8 // 12)
+
+print("f. ", end = "")
+print(8/0)
